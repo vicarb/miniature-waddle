45a2e50f0f882d8da2f79dba333e760a5e479dd4
vicarb <vcardenasb@protonmail.com>
2023-04-16 16:46:17 -0400

    update

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -10,8 +10,12 @@ def analyze_sentiment(text):
     sentiment = 'positive' if scores['compound'] > 0 else 'negative' if scores['compound'] < 0 else 'neutral'
     return sentiment
 
-text = input("Enter a sentence to analyze sentiment: ")
+while True:
+    text = input("Enter a sentence to analyze sentiment (press Q to quit): ")
+    
+    if text.lower() == "q":
+        break
 
-sentiment = analyze_sentiment(text)
+    sentiment = analyze_sentiment(text)
 
-print("Sentiment of the input text is: " + sentiment)
+    print("Sentiment of the input text is: " + sentiment)
